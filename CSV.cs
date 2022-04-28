5edfc7f97c6d1fe292161a2251843ea95d1e8304
shirish_chachadi <shirishcin@gmail.com>
2022-04-28 13:40:21 +0100

    initials

---
+++ CSV.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace CsvtoJson
+{
+    public class CSV
+    {
+        public string Header1 { get; set; } // This is an additional field added which will not be part of the json objects. This field is used to identy if this value is passed from Program to CsvRead method to convert this to json or not.
+        public int Header2 { get; set; }
+        public string Header3 { get; set; }
+        public string Header4 { get; set; }
+        public string Header5 { get; set; }
+        public string Header6 { get; set; }
+        public string Header7 { get; set; }
+        public string Header8 { get; set; }
+        public int Header9 { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using LINQtoCSV;
+using Newtonsoft.Json;
+
+namespace CsvtoJson
+{
+    internal class Program
+    {
+        public int count { get; set; }
+
+        public static void Main(string[] args)
+        {
+            var pr = new Program();
+
+            string? JsonToPass = pr.ReadCsvFile("Test1");  //Pass Test2 to see if the return json has only Test2 row's details
+            Console.WriteLine(JsonToPass);
+            if (pr.count == 0)
+            {
+                Console.WriteLine("No data for Test selected");
+            }
+            else
+            {
+                var FinalJson = JsonToPass.Replace("\"Header1\":null,", "");
+                Console.WriteLine(FinalJson);
+            }
+        }
+
+        public string? ReadCsvFile(string testid)
+        {
+            var csvFileDescription = new CsvFileDescription
+            {
+                FirstLineHasColumnNames = true,
+                IgnoreUnknownColumns = true,
+                SeparatorChar = ',',
+                UseFieldIndexForReadingData = false,
+            };
+            var csvContext = new CsvContext();
+            var rowsConfigs = csvContext.Read<CSV>("File.csv", csvFileDescription);
+            count = 0;
+            List<CSV> list = new List<CSV>();
+            foreach (var row in rowsConfigs)
+            {
+                if (row.Header1 == testid)
+                {
+                    count++;
+                    CSV cSV = new CSV
+                    {
+                        Header2 = row.Header2,
+                        Header3 = row.Header3,
+                        Header4 = row.Header4,
+                        Header5 = row.Header5,
+                        Header6 = row.Header6,
+                        Header7 = row.Header7,
+                        Header8 = row.Header8,
+                        Header9 = row.Header9,
+                    };
+
+                    list.Add(cSV);
+                }
+            }
+            if (count > 0)
+            {
+                Console.WriteLine("Number of locks found = " + count);
+                string newjson = JsonConvert.SerializeObject(list);
+                return newjson;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
